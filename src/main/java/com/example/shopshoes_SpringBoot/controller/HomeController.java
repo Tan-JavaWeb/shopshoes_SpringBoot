3af7fd907cf5a52c93a090c5b33c4926bbe5be2f
tranquangtanqt <tranquangtanqt1990@gmail.com>
2020-04-12 22:57:04 +0700

    index.html

---
+++ src/main/java/com/example/shopshoes_SpringBoot/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.shopshoes_SpringBoot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping(path = {"/", "/index", "home"})
+	public String HomePage(Model model) {
+
+		return "user/home";
+	}
+
+}

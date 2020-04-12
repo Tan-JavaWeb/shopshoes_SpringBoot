10e295306e6a4291410e0d6eca4d73d7764ea682
tranquangtanqt <tranquangtanqt1990@gmail.com>
2020-04-12 17:07:09 +0700

    first commit

---
+++ src/test/java/com/example/shopshoes_SpringBoot/ShopshoesSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shopshoes_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopshoesSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
